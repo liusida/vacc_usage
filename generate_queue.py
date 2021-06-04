38345af7fc2d5b23c411dc06e254c04db4753db5
sida <sliu1@uvm.edu>
2021-06-04 09:15:30 -0400

    Auto Save

--- generate_queue.py
+++ generate_queue.py
@@ -17,11 +17,11 @@ timestamp = datetime.timestamp(now)
 cluster_settings = {
     'bluemoon': {
         'name': 'bluemoon',
-        'partitions': ['ib', 'short', 'bigmemwk', 'bigmem', 'bluemoon*', 'week'],
+        'partitions': ['ib', 'short', 'bigmemwk', 'bigmem', 'bluemoon', 'bluemoon*', 'week'],
     },
     'deepgreen': {
         'name': 'deepgreen',
-        'partitions': ['dggpu*', 'dg-jup'],
+        'partitions': ['dggpu', 'dggpu*', 'dg-jup'],
     },
     'blackdiamond': {
         'name': 'bluemoon',
@@ -30,24 +30,26 @@ cluster_settings = {
 }
 
 def get_total_resources(cluster='bluemoon'):
-    df = pd.read_csv(f"{args.database}/{cluster_settings[cluster]['name']}.assets.txt", delim_whitespace=True, skiprows=1).drop_duplicates(subset='NODELIST')
+    c = cluster_settings[cluster]
+
+    df = pd.read_csv(f"{args.database}/{c['name']}.assets.txt", delim_whitespace=True).drop_duplicates(subset='NODELIST')
     df = df[df['PARTITION'].isin(cluster_settings[cluster]['partitions'])]
-    # print(df)
-    cpu = df['CPUS'].sum()
-    mem = df['MEMORY'].sum() / 1024.
-    gpu = 0
-    print(f"{cluster}: cpu: {cpu}, gpu: {gpu}, mem: {mem}")
+    df['GPU'] = df['GRES'].str.extract(r'gpu:(\d+)').fillna(0).astype(int)
+    ret = {
+        'cpu': df['CPUS'].sum(),
+        'gpu' : df['GPU'].sum(),
+        'mem': df['MEMORY'].sum() / 1024.,
+    }
+    return ret
 
+for key, value in cluster_settings.items():
+    value.update(get_total_resources(cluster=key))
 
-get_total_resources(cluster='blackdiamond')
-get_total_resources(cluster='deepgreen')
+def get_resources_amount(cluster='bluemoon', status='running'):
+    c = cluster_settings[cluster]
 
-exit()
-
-def get_resources_amount(cluster='bluemoon', partition='bluemoon', status='running'):
-
-    df = pd.read_csv(f"{args.database}/{cluster}.queue.txt", delim_whitespace=True)
-    df = df[df['PARTITION']==partition]
+    df = pd.read_csv(f"{args.database}/{c['name']}.queue.txt", delim_whitespace=True)
+    df = df[df['PARTITION'].isin(c['partitions'])]
     df['MEM_G'] = df['TRES_ALLOC'].str.extract(r'mem=(\d+)G').fillna(0).astype(float)
     df['MEM_M'] = df['TRES_ALLOC'].str.extract(r'mem=(\d+)M').fillna(0).astype(float)
     df['MEM'] = df['MEM_G'] + df['MEM_M']/1024.
@@ -58,76 +60,90 @@ def get_resources_amount(cluster='bluemoon', partition='bluemoon', status='runni
         df_status = df[df['EXEC_HOST'].notna()]
     else:
         df_status = df[df['EXEC_HOST'].isna()]
-    print(df_status.head())
-    # print(df_status)
+
+    # if cluster=='deepgreen' and status=='running':
+    #     print(c['partitions'])
+    #     print(df_status)
+
     cpu = df_status['CPU'].sum()
     gpu = df_status['GPU'].sum()
     mem = df_status['MEM'].sum()
-    print(f"{cluster}/{partition}/{status}:  cpu: {cpu}, gpu: {gpu}, mem: {mem}")
-    return cpu,gpu,mem
+    print(f"{cluster}/{status}:  cpu: {cpu}/{c['cpu']}, gpu: {gpu}/{c['gpu']}, mem: {mem}/{c['mem']}")
+    ret = {
+        f'{status}_CPU': cpu,
+        f'{status}_CPU_percentage': cpu / c['cpu'],
+        f'{status}_GPU': gpu,
+        f'{status}_GPU_percentage': 0 if c['gpu']==0 else gpu / c['gpu'],
+        f'{status}_MEM': mem,
+        f'{status}_MEM_percentage': mem/c['mem'],
+    }
+    return ret
 
-get_resources_amount(cluster='bluemoon', partition='bluemoon', status='waiting')
-get_resources_amount(cluster='bluemoon', partition='bluemoon', status='running')
-get_resources_amount(cluster='bluemoon', partition='short', status='waiting')
-get_resources_amount(cluster='bluemoon', partition='short', status='running')
-get_resources_amount(cluster='deepgreen', partition='dggpu', status='waiting')
-get_resources_amount(cluster='deepgreen', partition='dggpu', status='running')
-get_resources_amount(cluster='deepgreen', partition='dg-jup', status='waiting')
-get_resources_amount(cluster='deepgreen', partition='dg-jup', status='running')
+for key, value in cluster_settings.items():
+    value.update(get_resources_amount(cluster=key, status='waiting'))
+    value.update(get_resources_amount(cluster=key, status='running'))
 
+print(cluster_settings)
 
-exit()
+fig = make_subplots(rows=1, cols=3, subplot_titles=list(cluster_settings.keys()))
 
-plot_settings = [
-    [
-        {'title': '24h CPU Usage', 'measure': 'CPU', 'timespan': 24*60*60 },
-        {'title': '7d CPU Usage', 'measure': 'CPU', 'timespan': 7*24*60*60 },
-        {'title': '30d CPU Usage', 'measure': 'CPU', 'timespan': 30*24*60*60 },
-    ],
-    [
-        {'title': '24h GPU Usage', 'measure': 'GPU', 'timespan': 24*60*60 },
-        {'title': '7d GPU Usage', 'measure': 'GPU', 'timespan': 7*24*60*60 },
-        {'title': '30d GPU Usage', 'measure': 'GPU', 'timespan': 30*24*60*60 },
-    ],
-]
+resource_cols = ['CPU', 'GPU', 'MEM'][::-1]
+for i, (key,value) in enumerate(cluster_settings.items()):
+    fig.add_trace(
+        go.Bar(
+            name='Busy',
+            legendgroup = 'Busy',
+            y = resource_cols,
+            x = [value[f'running_{_c}_percentage'] for _c in resource_cols],
+            orientation='h',
+            marker=dict(
+                color='#f23c06',
+                # line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+            ),
+            showlegend=i==0,
+        ),
+        row=1, col=i+1,
+    )
+    fig.add_trace(
+        go.Bar(
+            name='Idle',
+            legendgroup='Idle',
+            y = resource_cols,
+            x = [1-value[f'running_{_c}_percentage'] for _c in resource_cols],
+            orientation='h',
+            marker=dict(
+                color='#cccccc',
+                # line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+            ),
+            showlegend=i==0,
+        ),
+        row=1, col=i+1,
+    )
+    fig.add_trace(
+        go.Bar(
+            name='Queue',
+            legendgroup='Queue',
+            y = resource_cols,
+            x = [value[f'waiting_{_c}_percentage'] for _c in resource_cols],
+            orientation='h',
+            marker=dict(
+                color='#fef764',
+                # line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+            ),
+            showlegend=i==0,
+        ),
+        row=1, col=i+1,
+    )
 
-subplot_titles = []
-for i in range(3):
-    for j in range(2):
-        subplot_titles.append(plot_settings[j][i]['title'])
-
-fig = make_subplots(rows=3, cols=2, subplot_titles=subplot_titles)
-
-for i in range(2):
-    for j in range(3):
-        p = plot_settings[i][j]
-        query_sql = f"SELECT user,lab, sum({p['measure']}Time) as measure FROM JOBS WHERE date>{timestamp-p['timespan']} GROUP BY user,lab ORDER BY measure DESC"
-        df = pd.read_sql_query(query_sql, con)
-        df['name'] = df['user'] + " / " + df['lab']
-        df['percentage'] = 100. * df['measure'] / df['measure'].sum()
-        df = df[df['percentage']>1.0]
-        df = df.append({
-                    'name': 'others',
-                    'measure': p['measure'],
-                    'percentage': 100.-df['percentage'].sum(),
-                }, ignore_index=True)
-        fig.add_trace(
-                go.Bar(
-                    x=df['name'], 
-                    y=df['percentage'], 
-                    text=df['percentage'].round(1).astype(str) + '%', 
-                    textposition='auto', 
-                    marker_color='#6287a2' if p['measure']=='CPU' else '#80c8bc',
-                ),
-                row=j+1, col=i+1,
-            )
-        fig.update_xaxes(title_text="Username / Lab", row=j+1, col=i+1)
-        fig.update_yaxes(title_text=f"{p['measure']} Percentage", row=j+1, col=i+1)
-
+fig.update_xaxes(
+    title_text='Percentage of total resources',
+    range=[0,5],
+)
 fig.update_layout(
-    showlegend=False,
+    barmode='stack',
+    # showlegend=False,
     font_size=16,
-    title=f"Recent Usage of VACC resources (Generated at {current_time})",
+    title=f"Current Resource Usage / Queue (Generated at {current_time})",
 )
 
 fig.write_html("public_html/queue.plots.html", default_height=300)
